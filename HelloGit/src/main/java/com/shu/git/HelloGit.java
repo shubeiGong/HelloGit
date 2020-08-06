976f37baac720647ccbab4aed62fde64f9c44183
gongshubei <18221929387@163.com>
2020-08-06 15:17:39 +0800

    version2

--- HelloGit/src/main/java/com/shu/git/HelloGit.java
+++ HelloGit/src/main/java/com/shu/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.shu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("version111");
     }
 }
