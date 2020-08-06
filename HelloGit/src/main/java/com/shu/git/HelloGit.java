47461d53fd0b6597748f962b3c6a6e7a49144ab1
gongshubei <18221929387@163.com>
2020-08-06 15:31:31 +0800

    主干添加

--- HelloGit/src/main/java/com/shu/git/HelloGit.java
+++ HelloGit/src/main/java/com/shu/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
         System.out.println("Hello git!");
         System.out.println("version111");
         System.out.println("创建分支");
+        System.out.println("主干添加");
 
     }
 }
