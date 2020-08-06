357db53855915586d01ecea00f1d43ea1e6befb0
gongshubei <18221929387@163.com>
2020-08-06 15:24:37 +0800

    创建分支

--- HelloGit/src/main/java/com/shu/git/HelloGit.java
+++ HelloGit/src/main/java/com/shu/git/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("version111");
+        System.out.println("创建分支");
+
     }
 }
